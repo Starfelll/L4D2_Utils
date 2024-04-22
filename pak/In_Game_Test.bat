2c5600ce3d43e993b6f0163f1266f527e3e859b7
Starfelll <daluobeizi@gmail.com>
2024-04-22 18:09:34 +0800

    usqc 1.6

--- pak/In_Game_Test.bat
+++ pak/In_Game_Test.bat
@@ -3,6 +3,6 @@ nekomdl -outdir "./" "../Compiling Files\QC/Bill_namvet.qc"
 @REM nekomdl -outdir "./" "../Compiling Files\QC_Arms/Bill_namvet_arms.qc"
 IF %ERRORLEVEL% NEQ 0 ( pause exit )
 call l4d2pak bill -out_dir "C:\Program Files (x86)\Steam\steamapps\common\Left 4 Dead 2\left4dead2\addons"
-echo "enter to start map tumtara"
+@REM echo "enter to start map tumtara"
 pause
-start "" "steam://rungameid/550//+map tumtara_l4d1//"
+@REM start "" "steam://rungameid/550//+map tumtara_l4d1//"
